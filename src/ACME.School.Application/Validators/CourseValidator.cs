d60ae4fe12586ad51a3145ba3d8d8ebaee50190e
Sebastian Capurro <sebacapu3@gmail.com>
2024-08-06 22:48:27 -0300

    add fluent validation and validate entities

---
+++ src/ACME.School.Application/Validators/CourseValidator.cs
@@ -0,0 +1,23 @@
+﻿using ACME.School.Domain.Entities;
+using FluentValidation;
+
+namespace ACME.School.Application.Validators
+{
+    public class CourseValidator : AbstractValidator<Course>
+    {
+        public CourseValidator()
+        {
+            RuleFor(course => course.Name)
+                .NotEmpty().WithMessage("The course name is required.");
+
+            RuleFor(course => course.RegistrationFee)
+                .GreaterThanOrEqualTo(0).WithMessage("The registration fee cannot be negative.");
+
+            RuleFor(course => course.StartDate)
+                .LessThan(course => course.EndDate).WithMessage("The start date must be less than the end date.");
+
+            RuleFor(course => course.EndDate)
+                .GreaterThan(course => course.StartDate).WithMessage("The end date must be greater than the start date.");
+        }
+    }
+}
---
+++ src/ACME.School.Application/Validators/StudentValidator.cs
@@ -0,0 +1,32 @@
+﻿using ACME.School.Domain.Entities;
+using FluentValidation;
+
+namespace ACME.School.Application.Validators
+{
+    public class StudentValidator : AbstractValidator<Student>
+    {
+        public StudentValidator()
+        {
+            RuleFor(student => student.Name)
+                        .NotEmpty().WithMessage("The student's name is required.");
+
+            RuleFor(student => student.BirthDate)
+                .NotEmpty().WithMessage("The student's date of birth is required.")
+                .Must(IsAdult).WithMessage("Only adults can register.");
+        }
+        private bool IsAdult(DateTime dateOfBith) 
+        {
+            var toDay = DateTime.Today;
+
+            if (dateOfBith <= toDay)
+            {
+                int edad = toDay.Year - dateOfBith.Year;
+
+                if (dateOfBith.Month > toDay.Month)
+                    --edad;
+                return edad >= 18;
+            }
+            return false;
+        }
+    }
+}
